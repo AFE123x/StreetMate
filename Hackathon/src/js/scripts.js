533f82a3fff045605be680e78bf675509adaf2df
An <pyop.van.an@gmail.com>
2023-11-11 02:54:47 -0500

    Javascript for Filter Buttons

--- Hackathon/src/js/scripts.js
+++ Hackathon/src/js/scripts.js
@@ -1,4 +1,13 @@
 
-document.querySelectorAll(".button").forEach(pill => {
-    pill.addEventListener("onclick", () => pill.classList.toggle(".button--selected")); 
-})
+var listingContainer = document.getElementsByClassName("listingContainer");
+var filterBox = listingContainer.getElementsByClassName("filterBox");
+var filterButtons = filterBox.getElementsByClassName("button");
+
+for (var i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("button--selected");
+        current[0].className =
+        current [0].className.replace("button--selected", "");
+        this.className += "button--selected"; 
+    })
+}
