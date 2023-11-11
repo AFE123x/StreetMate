8a4d7ee20f02c3b9f2e84af3e5df8e1f96efdaf3
Sandra-Cai <109099733+Sandra-Cai@users.noreply.github.com>
2023-11-11 04:23:25 -0500

    Update scripts.js

--- Hackathon/src/js/scripts.js
+++ Hackathon/src/js/scripts.js
@@ -1,13 +1,25 @@
 
-var listingContainer = document.getElementsByClassName("listingContainer");
-var filterBox = listingContainer.getElementsByClassName("filterBox");
-var filterButtons = filterBox.getElementsByClassName("button");
+
+var listingContainer = document.querySelector(".listingContainer");
+var filterButtons = listingContainer.querySelectorAll(".button");
 
 for (var i = 0; i < filterButtons.length; i++) {
-    filterButtons[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("button--selected");
-        current[0].className =
-        current [0].className.replace("button--selected", "");
-        this.className += "button--selected"; 
-    })
+  filterButtons[i].addEventListener("click", function () {
+    var current = listingContainer.querySelector(".button--selected");
+    current.classList.remove("button--selected");
+    this.classList.add("button--selected");
+    updateListing(this.innerText); 
+  });
+}
+
+function updateListing(filter) {
+  var listings = document.querySelectorAll('.listing');
+
+  listings.forEach(function(listing) {
+    if (filter === 'All' || listing.dataset.filter === filter) {
+      listing.style.display = 'block';
+    } else {
+      listing.style.display = 'none';
+    }
+  });
 }
